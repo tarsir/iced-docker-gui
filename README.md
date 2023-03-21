b21086180510756b2949397022747f4e5e61a1c7
Stephen Hara <stephen.hara@stephenhara.com>
2023-03-21 23:01:47 +0900

    Add a basic DockerImage struct and parse images output.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docker Management GUI
+
+A silly little project I'm working on to learn some GUI development stuff with
+Rust. It's going to let you do some management of Docker containers but I
+haven't really decided what that means yet!
--- src/docker.rs
+++ src/docker.rs
@@ -2,14 +2,47 @@ use std::process::Command;
 use std::vec::Vec;
 use std::error::Error;
 
-pub fn images() -> Result<Vec<String>, Box<dyn Error>> {
+#[derive(Debug)]
+pub struct DockerImage {
+    repository: String,
+    tag: String,
+    image_id: String,
+    size: String,
+    // TODO: make this a proper date/time type
+    created_at: String
+}
+
+pub fn images() -> Result<Vec<DockerImage>, Box<dyn Error>> {
     let output = Command::new("docker")
-        .arg("images")
+        .args(["images", "--format", "{{.ID}}:{{.Repository}}:{{.Tag}}:{{.Size}}:{{.CreatedAt}}"])
         .output()
         .expect("failed to get list of images");
 
     let output_str = String::from_utf8(output.stdout)?;
-    Ok(output_str.split('\n').map(String::from).collect::<Vec<String>>())
+    let output_lines = output_str
+        .split('\n')
+        .map(String::from)
+        .filter_map(|i: String| {
+            let parts = i
+                .splitn(5, ':')
+                .map(String::from)
+                .collect::<Vec<String>>();
+            let d_struct = 
+                match &parts[..] {
+                    [image_id, repository, tag, size, created_at] => Some(DockerImage {
+                        image_id: image_id.clone(),
+                        repository: repository.clone(),
+                        tag: tag.clone(),
+                        size: size.clone(),
+                        created_at: created_at.clone(),
+                    }),
+                    _ => None
+                };
+            d_struct
+        })
+        .collect::<Vec<DockerImage>>();
+    println!("{:?}", output_lines);
+    Ok(output_lines)
 }
 
 #[cfg(test)]
@@ -19,7 +52,7 @@ mod test {
 
     #[test]
     fn test_can_run_images() -> Result<(), Box<dyn Error>>{
-        images();
+        let image_list = images()?;
         Ok(())
     }
 }
